4044e3f061cbec9e0cbf1736a83be71ecb3aa101
Alejandro <alexaguilar281@gmail.com>
2024-07-20 06:20:03 +0200

    test: test email

--- src/auth/application/notification/recover-notification.ts
+++ src/auth/application/notification/recover-notification.ts
@@ -2,13 +2,16 @@ import { UserEmail } from '../../../users/domain/value-object/user-email';
 import { HttpInterface } from '../../../common/application/services/http-service';
 
 export class RecoverNotification {
-  constructor(private readonly http: HttpInterface) {}
+  constructor(private readonly http: HttpInterface) { }
 
   async execute(email: UserEmail, token: string): Promise<void> {
     this.http
       .post(`/notification/user/recover`, {
         email: email.value,
         token: token,
+      })
+      .then((e) => {
+        console.log(e)
       })
       .catch((e) => console.log(e));
   }
